1f942736ddc4fbac7540ebba2e2fca67c8a32f64
Timothy Wilks <91078140@mvusd.net>
2020-12-05 12:24:27 -0800

    Set up private node class

--- src/QueueData.java
+++ src/QueueData.java
@@ -1,4 +1,34 @@
 public class QueueData
 {
+    private class Node
+    {
+        private double data;
+        private Node link;
 
+        public Node(double data, Node link)
+        {
+            this.data = data;
+            this.link = link;
+        }
+
+        public double getData()
+        {
+            return data;
+        }
+
+        public Node getLink()
+        {
+            return link;
+        }
+
+        public void setData(int data)
+        {
+            this.data = data;
+        }
+
+        public String toString()
+        {
+            return "Data: " + data + ", Link: " + link + "";
+        }
+    }
 }
